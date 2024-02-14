2251d8523ffe7b86d3b0a6441f183473d1b38311
princethakur1999 <pk181299@gmail.com>
2024-02-14 10:30:12 +0530

    first commit -m

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -8,7 +8,10 @@ import axios from "axios";
 
 import { toast } from 'react-hot-toast';
 
-const BACKEND = "http://localhost:4000";
+// const BACKEND = "http://localhost:4000";
+
+const BACKEND = import.meta.env.VITE_REACT_APP_API_URL;
+
 
 
 export default function ForgotPassword() {
